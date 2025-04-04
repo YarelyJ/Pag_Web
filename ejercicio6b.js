8c3726d8327e31d8ecb2eee0f4ce6263b9938d37
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2025-04-04 01:02:51 +0000

    Add files via upload

---
+++ ejercicio6b.js
@@ -0,0 +1,9 @@
+const fs = require('node:fs/promises')
+fs.readFile('./archivo1b.txt') 
+.then(datos => { 
+    console.log(datos.toString()) 
+}) 
+.catch(error => { 
+    console.log(error) 
+})
+console.log('última línea del programa')
